d26f4001cb6e11af2dba7efede2591758d39ee5d
punchtalbes <hannes16@live.se>
2023-01-03 14:41:37 +0100

    ghjgjh

---
+++ src/Model/PlayerManager.java
@@ -0,0 +1,12 @@
+package Model;
+import java.util.ArrayList;
+
+public class PlayerManager {
+
+    private ArrayList<Player> highscore;
+
+    public PlayerManager(){
+       highscore = new ArrayList<>();
+    }
+
+}
