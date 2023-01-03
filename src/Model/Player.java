524aefae939029e2e76195aafa6a5aabf7a5ea77
punchtalbes <hannes16@live.se>
2023-01-03 14:33:31 +0100

    ghjgjh

---
+++ src/Model/Player.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Player {
+
+    private String name;
+    private int score;
+
+    public Player(String name, int score){
+        this.name=name;
+        this.score=score;
+
+    }
+}
