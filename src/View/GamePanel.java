0fc43a94804a901973ae869c3daa5fbcf6cb414b
sczcz <funkheim@hotmail.com>
2023-01-04 12:57:38 +0100

    hejhejhejhej

--- src/View/GamePanel.java
+++ src/View/GamePanel.java
@@ -17,6 +17,21 @@ public class GamePanel extends JPanel {
     private void createComponents() {
         setLayout(new GridLayout(11, 11));
 
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (i == 0 && j > 0) {
+                    // Add the column labels
+                    add(new JLabel("Column " + j, SwingConstants.CENTER));
+                } else if (j == 0 && i > 0) {
+                    // Add the row labels
+                    add(new JLabel("Row " + i, SwingConstants.CENTER));
+                } else {
+                    add(new JButton("Button (" + i + ", " + j + ")"));
+                }
+            }
+        }
+
+/*
         // Add labels for the rows
         for (int i = 1; i <= 10; i++) {
             add(new JLabel(String.valueOf(i), JLabel.CENTER));
@@ -31,5 +46,7 @@ public class GamePanel extends JPanel {
         for (int i = 1; i <= 100; i++) {
             add(new JButton(String.valueOf(i)));
         }
+
+ */
     }
 }
