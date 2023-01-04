1b118aee57a4721a136b0760ea6f3fad39b3dc86
sczcz <funkheim@hotmail.com>
2023-01-04 12:52:24 +0100

    hejhej

--- src/View/GamePanel.java
+++ src/View/GamePanel.java
@@ -30,20 +30,6 @@ public class GamePanel extends JPanel {
         // Add buttons
         for (int i = 1; i <= 100; i++) {
             add(new JButton(String.valueOf(i)));
-
-        /*for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 11; j++) {
-                if (i == 0 && j > 0) {
-                    // Add the column labels
-                    add(new JLabel("Column " + j, SwingConstants.CENTER));
-                } else if (j == 0 && i > 0) {
-                    // Add the row labels
-                    add(new JLabel("Row " + i, SwingConstants.CENTER));
-                } else {
-                    add(new JButton("Button (" + i + ", " + j + ")"));
-                }
-            }
-        }*/
         }
     }
 }
