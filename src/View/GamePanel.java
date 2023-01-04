c712ecdae5a637dbf571dde2956decfddd0826f0
sczcz <funkheim@hotmail.com>
2023-01-04 14:20:28 +0100

    grej

--- src/View/GamePanel.java
+++ src/View/GamePanel.java
@@ -26,46 +26,26 @@ public class GamePanel extends JPanel {
                 else if (i == 0 && j > 0) {
                     // Add the column labels
                     JLabel label = new JLabel("" + Character.toString(c), SwingConstants.CENTER);
-                    label.setSize(20,20);
-                    add(label);
+                    label.setPreferredSize(new Dimension(40, 40));
                     Font font = new Font("Arial", Font.BOLD, 12);
                     label.setFont(font);
+                    add(label);
                     c++;
-                    //add(new JLabel("" + Character.toString(c), SwingConstants.CENTER));
-                   // c++;
+
                 } else if (j == 0 && i > 0) {
                     // Add the row labels
                     JLabel labell = new JLabel(String.valueOf(i), SwingConstants.CENTER);
-                    labell.setSize(20,20);
-                    Font font = new Font("Arial", Font.BOLD, 18);
+                    labell.setPreferredSize(new Dimension(40, 40));
+                    Font font = new Font("Arial", Font.BOLD, 12);
                     labell.setFont(font);
                     add(labell);
-                    //add(new JLabel(String.valueOf(i), SwingConstants.CENTER));
 
                 } else {
                     JButton button = new JButton("");
-                    button.setSize(20,20);
+                    button.setPreferredSize(new Dimension(40,40));
                     add(button);
                 }
             }
         }
-
-/*
-        // Add labels for the rows
-        for (int i = 1; i <= 10; i++) {
-            add(new JLabel(String.valueOf(i), JLabel.CENTER));
-        }
-
-        // Add labels for the columns
-        for (int i = 0; i < 10; i++) {
-            add(new JLabel(String.valueOf((char) ('A' + i)), JLabel.CENTER));
-        }
-
-        // Add buttons
-        for (int i = 1; i <= 100; i++) {
-            add(new JButton(String.valueOf(i)));
-        }
-
- */
     }
 }
