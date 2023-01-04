706bf943ed5067140ba701ca86a2e0f6736df59a
sczcz <funkheim@hotmail.com>
2023-01-04 13:33:55 +0100

    LOOK AND FEEL

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -2,8 +2,15 @@ package Controller;
 
 import Model.Spelplan1;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         Controller controller = new Controller();
 
     }
